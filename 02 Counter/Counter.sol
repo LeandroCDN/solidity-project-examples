65e4a7d27c9da413eae32e80a4de647f7f0f6420
Leandro <leandro@Air-de-Leandro.fibertel.com.ar>
2023-04-10 10:00:01 -0300

    Counter code

---
+++ 02 Counter/Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint256 count;
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
+    function incrementCount() public {
+        count++;
+    }
+
+    function decrementCount() public {
+        count--;
+    }
+}
