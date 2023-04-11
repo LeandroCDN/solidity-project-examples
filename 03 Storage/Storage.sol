03cc47ee91858b9cfd9e45a9edbf244a3637ef76
Leandro <leandro@Air-de-Leandro.fibertel.com.ar>
2023-04-11 10:20:49 -0300

    Add Storage Contract Code

---
+++ 03 Storage/Storage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+//Aqui van las importaciones
+
+contract Storage {
+
+    uint256 number;
+
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
