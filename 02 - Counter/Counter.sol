a6401789efaa4bce1369c4d03df574a9c07eaf4b
LeanLabiano <leandrodenos@gmail.com>
2024-02-23 15:12:13 -0300

    Nostros resolvimos los ejercicios de Counter

--- 02 - Counter/Counter.sol
+++ 02 - Counter/Counter.sol
@@ -3,16 +3,38 @@ pragma solidity ^0.8.0;
 
 contract Counter {
     uint256 count;
+    uint public maxCount;
+    uint public minCount;
+
+    constructor(uint maxCount_, uint minCount_){
+        maxCount = maxCount_;
+        minCount = minCount_;
+    }
 
     function getCount() public view returns (uint256) {
         return count;
     }
 
     function incrementCount() public {
+        require(count < maxCount, "maxCount error");
         count++;
     }
 
     function decrementCount() public {
+
+        require(count > minCount, "minCount error");
         count--;
+    }
+
+    function setMaxAndMinCount(uint maxCount_, uint minCount_) public {
+        maxCount = maxCount_;
+        minCount = minCount_;
+    }
+
+    function setCount(uint newValueForCount) public {
+        require(newValueForCount > minCount, "minCount error");
+        require(newValueForCount < maxCount, "maxCount error");
+        
+        count = newValueForCount;
     }
 }
